3d67f3640f50f8b8defa07a963623fc429aec4b1
Wassimsghaier420 <52701776+Wassimsghaier420@users.noreply.github.com>
2019-10-04 14:43:25 +0100

    Updates

---
+++ precache-manifest.ad93dbc0935af35aadfc2d36f5406839.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c84057af9718378b9b41675525952e",
+    "url": "/HOC/index.html"
+  },
+  {
+    "revision": "fa5f28297528852d6bf6",
+    "url": "/HOC/static/css/main.47b84665.chunk.css"
+  },
+  {
+    "revision": "40a6b4a1d5777185bb7b",
+    "url": "/HOC/static/js/2.63130d7f.chunk.js"
+  },
+  {
+    "revision": "fa5f28297528852d6bf6",
+    "url": "/HOC/static/js/main.1308f294.chunk.js"
+  },
+  {
+    "revision": "61ab6fa5d3dd1e3d7df2",
+    "url": "/HOC/static/js/runtime-main.4a8aa494.js"
+  },
+  {
+    "revision": "ae8038aedf5db9b56f5295f8a6b19efb",
+    "url": "/HOC/static/media/drive.ae8038ae.jpg"
+  },
+  {
+    "revision": "32e10f4a5ac9979853fef153ac46afa1",
+    "url": "/HOC/static/media/fightclub.32e10f4a.jpg"
+  },
+  {
+    "revision": "4b00deb6a753c8713d4d3f67b425f59c",
+    "url": "/HOC/static/media/historyx.4b00deb6.jpg"
+  },
+  {
+    "revision": "72ad4bdb2a0a20fc7cb91a2b4e788689",
+    "url": "/HOC/static/media/shawshank.72ad4bdb.jpg"
+  },
+  {
+    "revision": "60df5080adc2103acf2b91738fecfa61",
+    "url": "/HOC/static/media/trainspotting.60df5080.jpg"
+  }
+]);
